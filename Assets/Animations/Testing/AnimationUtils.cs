fde2652b06115cba055a37df9dc1e38c67cd75f5
Ne0Star <danil.prokopev.00@gmail.com>
2022-09-17 16:52:59 +0500

    да

--- Assets/Animations/Testing/AnimationUtils.cs
+++ Assets/Animations/Testing/AnimationUtils.cs
@@ -19,89 +19,82 @@ public class AnimationData : ScriptableObject
 }
 
 
-[CustomEditor(typeof(AnimationUtils))]
-public class AnimationEditor : Editor
-{
-    AnimationUtils tt;
-    Camera cam;
-    private void OnEnable()
-    {
-        tt = (AnimationUtils)target;
-    }
+//[CustomEditor(typeof(AnimationUtils))]
+//public class AnimationEditor : Editor
+//{
+//    AnimationUtils tt;
+//    Camera cam;
+//    private void OnEnable()
+//    {
+//        tt = (AnimationUtils)target;
+//    }
 
-    public override void OnInspectorGUI()
-    {
-        var FileName = serializedObject.FindProperty("fileName");
-        var FilePath = serializedObject.FindProperty("relativePath");
-        var StateName = serializedObject.FindProperty("stateName");
-        var Animator = serializedObject.FindProperty("animator");
-        var Camera = serializedObject.FindProperty("screenCamera");
-        EditorGUILayout.PropertyField(FileName);
-        EditorGUILayout.PropertyField(FilePath);
-        EditorGUILayout.PropertyField(StateName);
-        EditorGUILayout.PropertyField(Animator);
-        EditorGUILayout.PropertyField(Camera);
-        serializedObject.ApplyModifiedProperties();
+//    public override void OnInspectorGUI()
+//    {
+//        var FileName = serializedObject.FindProperty("fileName");
+//        var FilePath = serializedObject.FindProperty("relativePath");
+//        var StateName = serializedObject.FindProperty("stateName");
+//        var animator = serializedObject.FindProperty("animator");
+//        var camera = serializedObject.FindProperty("screenCamera");
+//        EditorGUILayout.PropertyField(FileName);
+//        EditorGUILayout.PropertyField(FilePath);
+//        EditorGUILayout.PropertyField(StateName);
+//        EditorGUILayout.PropertyField(animator);
+//        EditorGUILayout.PropertyField(camera);
+//        serializedObject.ApplyModifiedProperties();
 
-        if (GUILayout.Button("Начать запись"))
-        {
+//        if (GUILayout.Button("Начать запись"))
+//        {
+//            Animator anim = (Animator)animator.GetValue();
+//            anim.Play((string)StateName.GetValue(),0);
+//            //ToTexture2D((Camera)camera.GetValue(), (s) => SaveSpriteToEditorPath(s, (string)FilePath.GetValue()));
+//            //WaitSprite((Camera)camera.GetValue(), (string)FilePath.GetValue());
+//            //DelayUseAsync(250, (Camera)Camera.GetValue(), (string)FilePath.GetValue());
+//            // StartCoroutine(Test());
+//        }
+//    }
 
-            DelayUseAsync(250, (Camera)Camera.GetValue(), (string)FilePath.GetValue());
-            // StartCoroutine(Test());
-        }
-    }
-    async static void DelayUseAsync(int timeStep, Camera cam, string path)
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            Sprite s = ToTexture2D(cam);
-            SaveSpriteToEditorPath(s, path);
-            await Task.Delay(timeStep);
+//    private void ToTexture2D(Camera cam, System.Action<Sprite> onComplete)
+//    {
+//        RenderTexture rTex = cam.targetTexture;
+//        RenderTexture currentActiveRT = RenderTexture.active;
+//        RenderTexture.active = rTex;
+//        cam.Render();
+//        Rect rect = new Rect(0, 0, rTex.width, rTex.height);
+//        Texture2D tex = new Texture2D(rTex.width, rTex.height);
+//        tex.ReadPixels(rect, 0, 0);
+//        tex.Apply();
+//        RenderTexture.active = currentActiveRT;
+//        Sprite s = Sprite.Create(tex, rect, new Vector2(0.5f, 0.5f));
+//        onComplete(s);
+//    }
 
-        }
+//    Sprite SaveSpriteToEditorPath(Sprite sprite, string proj_path)
+//    {
+//        var abs_path = Path.Combine(Application.dataPath, proj_path);
+//        proj_path = Path.Combine("Assets", proj_path);
 
-    }
-    public static Sprite ToTexture2D(Camera cam)
-    {
-        RenderTexture rTex = cam.targetTexture;
-        RenderTexture currentActiveRT = RenderTexture.active;
-        RenderTexture.active = rTex;
-        cam.Render();
-        Rect rect = new Rect(0, 0, rTex.width, rTex.height);
-        Texture2D tex = new Texture2D(rTex.width, rTex.height);
-        tex.ReadPixels(rect, 0, 0);
-        tex.Apply();
-        RenderTexture.active = currentActiveRT;
-        Sprite s = Sprite.Create(tex, rect, new Vector2(0.5f, 0.5f));
-        return s;
-    }
+//        Directory.CreateDirectory(Path.GetDirectoryName(abs_path));
+//        File.WriteAllBytes(abs_path, ImageConversion.EncodeToPNG(sprite.texture));
 
-    public static void SaveSpriteToEditorPath(Sprite sprite, string proj_path)
-    {
-        var abs_path = Path.Combine(Application.dataPath, proj_path);
-        proj_path = Path.Combine("Assets", proj_path);
+//        AssetDatabase.Refresh();
 
-        Directory.CreateDirectory(Path.GetDirectoryName(abs_path));
-        File.WriteAllBytes(abs_path, ImageConversion.EncodeToPNG(sprite.texture));
+//        var ti = AssetImporter.GetAtPath(proj_path) as TextureImporter;
+//        ti.spritePixelsPerUnit = sprite.pixelsPerUnit;
+//        ti.mipmapEnabled = false;
+//        ti.textureType = TextureImporterType.Sprite;
 
-        AssetDatabase.Refresh();
+//        EditorUtility.SetDirty(ti);
+//        ti.SaveAndReimport();
 
-        var ti = AssetImporter.GetAtPath(proj_path) as TextureImporter;
-        ti.spritePixelsPerUnit = sprite.pixelsPerUnit;
-        ti.mipmapEnabled = false;
-        ti.textureType = TextureImporterType.Sprite;
-
-        //EditorUtility.SetDirty(ti);
-        //ti.SaveAndReimport();
-
-
-        //var r = AssetDatabase.LoadAssetAtPath<Sprite>(proj_path);
-        //return r;
-    }
-}
+//        return AssetDatabase.LoadAssetAtPath<Sprite>(proj_path);
+//    }
+//}
 
 public class AnimationUtils : MonoBehaviour
 {
+    [SerializeField] private List<string> test;
+
     [SerializeField] private bool startRecord = false;
     [SerializeField] private bool block = false;
     [SerializeField] private Camera screenCamera;
@@ -114,75 +107,80 @@ public class AnimationUtils : MonoBehaviour
 
 
     [Header("Анимация для раскадровки: ")]
-    [Space(2)]
+    [Space(1)]
     [Header("Название анимации в Animator")]
     [SerializeField] private string stateName;
     [Header("Animator")]
     [SerializeField] private Animator animator;
 
-    //private void Awake()
-    //{
-    //    animator = gameObject.GetComponent<Animator>();
+    private void Awake()
+    {
+        if(!animator)
+        animator = gameObject.GetComponent<Animator>();
 
-    //}
+    }
 
-    //private IEnumerator Start()
-    //{
-    //    results.Clear();
-    //    yield return new WaitForSeconds(2f);
-    //    animator.Play(stateName);
-    //}
-    //[Range(0.02f, 1f)]
-    //[SerializeField] private float frameStep;
-    //[SerializeField] private bool isRecording = false;
+    private IEnumerator Start()
+    {
+        results.Clear();
+        yield return new WaitForSeconds(1f);
+        animator.Play(stateName);
+        StartRecord();
+        yield return new WaitForEndOfFrame();
+        //Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName("empty"));
+        yield return new WaitUntil(() => animator.GetCurrentAnimatorStateInfo(0).IsName("empty"));
+        StopRecord();
+    }
+    [Range(0.02f, 1f)]
+    [SerializeField] private float frameStep;
+    [SerializeField] private bool isRecording = false;
 
-    //private IEnumerator Recording()
-    //{
-    //    while (isRecording)
-    //    {
-    //        StartCoroutine(CreateFrame());
+    private IEnumerator Recording()
+    {
+        while (isRecording)
+        {
+            StartCoroutine(CreateFrame());
 
-    //        yield return new WaitForSeconds(frameStep);
-    //    }
-    //}
+            yield return new WaitForSeconds(frameStep);
+        }
+    }
 
-    //private IEnumerator CreateFrame()
-    //{
-    //    StartCoroutine(ToTexture2D(screenCamera, (s) => results.Add(s)));
-    //    //Sprite result = ToTexture2D(screenCamera, "Frame" + results.Count);
-    //    //yield return new WaitUntil(() => result);
-    //    //results.Add(result);
-    //    yield break;
-    //}
+    private IEnumerator CreateFrame()
+    {
+        StartCoroutine(ToTexture2D(screenCamera, (s) => results.Add(s)));
+        //Sprite result = ToTexture2D(screenCamera, "Frame" + results.Count);
+        //yield return new WaitUntil(() => result);
+        //results.Add(result);
+        yield break;
+    }
 
-    //public void StopRecord()
-    //{
-    //    isRecording = false;
+    public void StopRecord()
+    {
+        isRecording = false;
 
-    //    StartCoroutine(SaveResult());
+        StartCoroutine(SaveResult());
 
-    //}
+    }
 
-    //private IEnumerator SaveResult()
-    //{
-    //    for (int i = 0; i < results.Count; i++)
-    //    {
-    //        Sprite s = results[i];
-    //        if (s && s != null)
-    //        {
-    //            results[i] = SaveSpriteToEditorPath(s, relativePath + "/" + fileName + "_" + i + ".png");
-    //            yield return new WaitForEndOfFrame();
-    //        }
-    //    }
-    //}
+    private IEnumerator SaveResult()
+    {
+        for (int i = 0; i < results.Count; i++)
+        {
+            Sprite s = results[i];
+            if (s && s != null)
+            {
+                results[i] = SaveSpriteToEditorPath(s, relativePath + "/" + fileName + "_" + i + ".png");
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
 
-    //public void StartRecord()
-    //{
-    //    if (block) return;
-    //    isRecording = true;
-    //    StartCoroutine(Recording());
-    //}
-
+    public void StartRecord()
+    {
+        if (block) return;
+        isRecording = true;
+        StartCoroutine(Recording());
+    }
 
     private IEnumerator ToTexture2D(Camera cam, System.Action<Sprite> onComplete)
     {
@@ -223,29 +221,10 @@ public class AnimationUtils : MonoBehaviour
         var r = AssetDatabase.LoadAssetAtPath<Sprite>(proj_path);
         return r;
     }
-    public void tt()
-    {
-        StartCoroutine(Test());
-    }
-    private IEnumerator Test()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            Debug.Log("test");
-            yield return new WaitForSeconds(0.01f);
-        }
-    }
 
     private void OnDrawGizmos()
     {
         if (!screenCamera) screenCamera = GameObject.FindGameObjectWithTag("ScreenCamera").GetComponent<Camera>();
-
-        if (startRecord)
-        {
-            StartCoroutine(Test());
-            startRecord = false;
-        }
-
     }
 
 }
